f2eb0411032c43f41dfcc1a8b168012726b96603
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-05-31 06:23:31 +0200

    Create ex12.py

---
+++ learningpythonthehardway/ex12.py
@@ -0,0 +1,7 @@
+age = raw_input("How old are you brother ?") # will get some text ;def
+height = raw_input("How tall are you ?")
+eat = raw_input("do you eat enough ?")
+
+print "So, you're a %r years old and %r tall guy that says : '%r' to the food, right ?" % (age, height, eat)
+
+# Nb: to get a number from the return stuff, 'x =  int(raw_input())'

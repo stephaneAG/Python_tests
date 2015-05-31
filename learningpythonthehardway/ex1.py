090e843ccc73d8f6feb4281a0c1faa73a3f805be
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-05-31 06:20:51 +0200

    Create ex1.py

---
+++ learningpythonthehardway/ex1.py
@@ -0,0 +1,8 @@
+# remember: to run: 'python ex1.py'
+
+print "This is a Hello World"
+print "Hello world again"
+print "I am doing this *** excercice"
+print "I'm wondering how long it will take ..."
+print ".. to ba able to run a little serial app in python .."
+print "But Who Knows ?!"

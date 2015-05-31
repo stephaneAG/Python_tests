e6a10b43cfe3bb220b0f2067c6541246a3988cfa
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-05-31 06:22:17 +0200

    Create ex6.py

---
+++ learningpythonthehardway/ex6.py
@@ -0,0 +1,6 @@
+# remember: to run: 'python ex6.py'
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny ? %r" # '%r' is like '%s', but stands for 'raw' (> to use for debugging (..) ) 
+
+print joke_evaluation % hilarious

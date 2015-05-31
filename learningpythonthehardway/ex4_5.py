afe6df86099a547d89ece15c1f18325a71201c38
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-05-31 06:21:48 +0200

    Create ex4_5.py

---
+++ learningpythonthehardway/ex4_5.py
@@ -0,0 +1,13 @@
+# remember: to run: 'python ex4.py'
+
+cars = 100
+my_name = 'Tef'
+my_age = 25 # years ;p
+my_brain_is = 'Fucked up with data'
+my_stuff = 'I do Stuff'
+
+print "I got", cars, "cars behind my house"
+
+print "Hi. My name's %s" % my_name
+print "I am %d years old, my brain is currently '%s' , and I hope I'll continue to say out loud (and proud): %s" % (my_age, my_brain_is, my_stuff)
+print "And if I go outta roads and try to mix stuff , error ? '%s' -> : %s" % (my_name, my_brain_is + ',' + my_stuff ) # nb: semms to also works with ' "" ' (..)
